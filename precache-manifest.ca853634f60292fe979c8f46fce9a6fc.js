be888e313a37e4e20f3debff8833e5def6156fbd
UmangGPatel <1998patelumang@gmail.com>
2019-03-07 19:50:02 +0530

    Updates

---
+++ precache-manifest.ca853634f60292fe979c8f46fce9a6fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f108eda67a31d924f9e",
+    "url": "/React-Live-TV-Template/static/css/main.b33a8d7a.chunk.css"
+  },
+  {
+    "revision": "9f108eda67a31d924f9e",
+    "url": "/React-Live-TV-Template/static/js/main.9f108eda.chunk.js"
+  },
+  {
+    "revision": "363d9f54498e53cc892e",
+    "url": "/React-Live-TV-Template/static/js/runtime~main.363d9f54.js"
+  },
+  {
+    "revision": "65987e4bf4a0bb72a5fb",
+    "url": "/React-Live-TV-Template/static/js/2.65987e4b.chunk.js"
+  },
+  {
+    "revision": "71237b6b529beea8c41d7e7397752d8f",
+    "url": "/React-Live-TV-Template/index.html"
+  }
+];
